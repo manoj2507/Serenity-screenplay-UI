4914a9bc124f62b124830e5f4277a96269b0b26c
Manoj Kumar <manoganga1151@gmail.com>
2021-10-17 23:49:24 +0100

    Sonar configuration

--- src/test/java/com/test/shop/stepdefinitions/ProductAvailability.java
+++ src/test/java/com/test/shop/stepdefinitions/ProductAvailability.java
@@ -3,10 +3,12 @@ package com.test.shop.stepdefinitions;
 import com.test.shop.tasks.NavigateTo;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class ProductAvailability {
     @Before
@@ -14,10 +16,15 @@ public class ProductAvailability {
         OnStage.setTheStage(new OnlineCast());
     }
 
-    @Given("that {string} wants to check the availability of {string} Jeans")
-    public void thatStringwantstochecktheavailabilityofstringjeans(String actor,String brand){
+    @Given("that {string} wants to check the availability of Jeans brands")
+    public void thatStringwantstochecktheavailabilityofstringjeans(String actor){
         theActorCalled(actor).attemptsTo(NavigateTo.Myntra());
 
+    }
+
+    @When("When he tries to search the {string} brand in Myntra")
+    public void whenhetriestosearchthestringbranchinmyntra(String brand){
+        theActorInTheSpotlight().attemptsTo();
     }
 
 
